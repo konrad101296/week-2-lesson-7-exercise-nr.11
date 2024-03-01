752ed3e042cdc74b45e38b3ddce9104425cc6e34
konrad101296 <konradslimak@o2.pl>
2024-03-01 18:53:09 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.11/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("wpisz ocene:");
+int ocena = int.Parse(Console.ReadLine());
+
+if (ocena == 1)
+{
+    Console.WriteLine("niedostateczny");
+}
+else if (ocena == 2)
+{
+    Console.WriteLine("dopuszczajacy");
+}
+else if (ocena == 3)
+{
+    Console.WriteLine("dostateczny");
+}
+else if (ocena == 4)
+{
+    Console.WriteLine("dobry");
+}
+else if (ocena == 5)
+{
+    Console.WriteLine("bardzo dobry");
+}
+else if (ocena == 6)
+{
+    Console.WriteLine("celujacy");
+}
+else
+{
+    Console.WriteLine("oceny sa tylko od 1 do 6");
+}
