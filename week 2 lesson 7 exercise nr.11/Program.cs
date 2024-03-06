262ea635843585c3c60af6609660109d4dbb371a
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-03-06 16:52:21 +0100

    Update Program.cs
    zadanie zrobiono dodatkowo używajac switch

--- week 2 lesson 7 exercise nr.11/Program.cs
+++ week 2 lesson 7 exercise nr.11/Program.cs
@@ -29,3 +29,31 @@ else
 {
     Console.WriteLine("oceny sa tylko od 1 do 6");
 }
+
+Console.WriteLine("wpisz ocene:");
+int ocena = int.Parse(Console.ReadLine());
+
+switch (ocena)
+{
+    case 1:
+        Console.WriteLine("niedostateczny");
+        break;
+    case 2:
+        Console.WriteLine("dopuszczajacy");
+        break;
+    case 3:
+        Console.WriteLine("dostateczny");
+        break;
+    case 4:
+        Console.WriteLine("dobry");
+        break;
+    case 5:
+        Console.WriteLine("bardzo dobry");
+        break;
+    case 6:
+        Console.WriteLine("celujacy");
+        break;
+    default:
+        Console.WriteLine("oceny sa tylko od 1 do 6");
+        break;
+}
